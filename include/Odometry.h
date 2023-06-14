53cebb1c5b124f64cfdd4bbc3a57cf7d08971e9d
Patrick S <111544394+Glutinfree@users.noreply.github.com>
2023-06-14 15:38:02 -0700

    Create Odometry.h

---
+++ include/Odometry.h
@@ -0,0 +1,18 @@
+/**
+* Header file for odometry and stuff
+*
+* Two Motion Encoders, x and y axis
+*
+*
+*/
+
+#include "api.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void writeOdometry(void);
+void readOdometry(void);
+#ifdef __cplusplus
+}
+#endif
