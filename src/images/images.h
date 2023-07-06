948ac618df2cd51224f87c8d2276cba1156c3d85
CYocum27 <cjyocum27@chaminet.org>
2023-07-06 09:50:44 -0700

    Added missing files from merge
    - added the background image source and header files

---
+++ src/images/images.h
@@ -0,0 +1,9 @@
+#include "display/lvgl.h"
+#include <stdint.h>
+#include "display/lv_conf.h"
+
+extern const uint8_t background_map[];
+extern const lv_img_dsc_t background;
+
+extern const uint8_t init_map[];
+extern const lv_img_dsc_t init;
