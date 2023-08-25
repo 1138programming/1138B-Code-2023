9d827291e9d9cca4a4bc5e236af78edec2817ff6
JAKlein26 <111544394+Glutinfree@users.noreply.github.com>
2023-08-24 17:18:49 -0700

    vision psuedo code ( not finished)

--- src/Vision.cpp
+++ src/Vision.cpp
@@ -3,7 +3,14 @@
 
 
 Vision::Vision() {
+    
     Eye.takeSnapshot(Eye__SIG_1);
+
     Obxcord = Eye.objects[0].angle;
     Count = Eye.objectCount;
+
 }
+
+double Vision::Getdist() {
+    //return Tan(OBxcord * (400/FOV))*height
+};
